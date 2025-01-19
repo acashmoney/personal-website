203a842d394e3cd62d94bc675aa7043395731e6a
Aakaash Meduri <aakaash.meduri@gmail.com>
2025-01-19 04:53:28 -0500

    more clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { motion } from 'framer-motion';
-import { Inter } from '@next/font/google';
+import Head from 'next/head';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <motion.div
-        key={router.route}
-        initial="initial"
-        animate="animate"
-        variants={{
-          initial: {
-            opacity: 0,
-          },
-          animate: {
-            opacity: 1,
-          },
-        }}
-      >
-        <main className={`${inter.className} min-h-screen flex flex-col`}>
-          <Component {...pageProps} />
-        </main>
-      </motion.div>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/A.svg" />
+      </Head>
+      <main className="min-h-screen flex flex-col">
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
